2cc3152df289b020923b1c41e78a31cec4fbc18c
Ubeyd-Sahin <115145402+Ubeyd-Sahin@users.noreply.github.com>
2023-10-12 12:46:19 +0200

    Bestanden verplaatst!
    bestanden overgeplaatst van ander map

---
+++ src/components/TodoList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>To-Do Lijst</h1>
+        <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Maak een task!" />
+        <ul>
+            <li v-for="(todo, index) in todos" :key="index">
+                {{ todo.text }}
+                <button @click="removeTodo(index)">Verwijder</button>
+            </li>
+        </ul>
+    </div>
+</template>
+  
+
+<script>
+export default {
+    data() {
+        return {
+            todos: [], 
+            newTodo: ''
+        };
+    },
+    methods: {
+        addTodo() {
+            this.todos.push({ text: this.newTodo, completed: false });
+            this.newTodo = ''; 
+        },
+        removeTodo(index) {
+            this.todos.splice(index, 1);
+        } 
+    }
+};
+</script>
